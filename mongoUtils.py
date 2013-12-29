8b5c2541b60a017db091eb0c1e6ca939b90115a4
uchida <uchida.atsushi.8@gmail.com>
2013-12-30 03:20:35 +0900

    fix

--- mongoUtils.py
+++ mongoUtils.py
@@ -1,1 +1,13 @@
 from pymongo import MongoClient
+
+class Mongo:
+    '''
+    Mongo
+    '''
+    def __init__(self, addr):
+        self.addr = addr
+        self.conn = MongoClient(self.addr)
+        self.db   = self.conn.test
+
+    def getErrorData(errorMessage):
+        return errorMessage
--- pushErrorData.py
+++ pushErrorData.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from postUtils import *
+from mongoUtils import *
 
 url = "http://10.211.55.4:3001/api/3.0"
 method = "getLatestData"
@@ -11,3 +12,5 @@ a = postData(rpc, url)
 print a["result"]
 
 mongo_ip = "10.211.55.4"
+m = Mongo(mongo_ip)
+count = m.getErrorDataCount("struct")
